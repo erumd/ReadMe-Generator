ee3f3d006fb99070036c9afc3f6b52ec13451cb8
Erum Dhukka <erumdhukka531@gmail.com>
2021-02-07 18:19:23 -0600

    deleted template,js

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -7,29 +7,11 @@ function renderLicenseBadge(license) {
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  // if (condition) {
-  // } else {
-  // }
-  // if (data.license === MIT) {
-  //     MIT License
-  //     Copyright (C) <year>  <name of author>
-  //     Permission is hereby granted, free of charge, to any person obtaining a copy
-  //     of this software and associated documentation files (the "Software"), to deal
-  //     in the Software without restriction, including without limitation the rights
-  //     to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-  //     copies of the Software, and to permit persons to whom the Software is
-  //     furnished to do so, subject to the following conditions:
-  //     The above copyright notice and this permission notice shall be included in all
-  //     copies or substantial portions of the Software.
-  //     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-  //     IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-  //     FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-  //     AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-  //     LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-  //     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-  //     SOFTWARE.
-  // } else {
-  // }
+  if (license) {
+    license === none;
+    return `Choose a license `
+    
+  }
 }
 
 // TODO: Create a function that returns the license section of README
